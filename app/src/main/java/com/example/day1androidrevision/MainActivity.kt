e37ea8d6ec0249f6faed61215cb6ce6eb7e2a06e
TharanginiGajendran <tharanginig1998@gmail.com>
2023-12-18 18:04:49 +0530

    2nd commit

--- app/src/main/java/com/example/day1androidrevision/MainActivity.kt
+++ app/src/main/java/com/example/day1androidrevision/MainActivity.kt
@@ -12,19 +12,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewListeners()
 
+
+    }
+
+    private fun viewListeners() {
         val enterName = findViewById<EditText>(R.id.et_user_name)
         val submitName = findViewById<Button>(R.id.submit_btn)
         val message = findViewById<TextView>(R.id.welcome_msg)
+
         submitName.setOnClickListener {
-
-            var text = "Welcome ${enterName.text.toString()}"
-            message.text = text
-
-//            Toast.makeText(this.applicationContext, text, Toast.LENGTH_LONG).show()
-
+            var name = enterName.text.toString()
+            if (name == "") {
+                message.text = "The Name should not be empty"
+                Toast.makeText(this@MainActivity, "please enter your name", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                var text = "Welcome ${name}"
+                message.text = text
+                enterName.text.clear()
+            }
         }
-    }
 
+    }
 
 }
